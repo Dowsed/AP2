3feff8754d86d7638e2d730666ee2c0fe5dc85a3
Peter Dowsett <peter.dowsett@relab.dk>
2021-10-07 18:03:54 +0100

    Added Hello World xcode project

---
+++ Hello World/Hello World/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hello World
+//
+//  Created by Pete Dowsett on 07/10/2021.
+//  Copyright © 2021 Pete Dowsett. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
